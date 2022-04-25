caf546f8c9e589168b72ec1ce2e7a7638439aa51
lawshop <itpjh@naver.com>
2022-04-25 11:40:16 +0900

    Initial commit

---
+++ src/main/java/kr/co/codalserver/CodalServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.codalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodalServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodalServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/codalserver/CodalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.codalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodalServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
