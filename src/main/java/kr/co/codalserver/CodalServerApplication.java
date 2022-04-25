faa818a0d81bee07bd2d20f9930a6b09474de4aa
lawshop <itpjh@naver.com>
2022-04-25 11:45:37 +0900

    테스트

--- src/main/java/kr/co/codalserver/CodalServerApplication.java
+++ src/main/java/kr/co/codalserver/CodalServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodalServerApplication {
-
+    // 수정 중
     public static void main(String[] args) {
         SpringApplication.run(CodalServerApplication.class, args);
     }
